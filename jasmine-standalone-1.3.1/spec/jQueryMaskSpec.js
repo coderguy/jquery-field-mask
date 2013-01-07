0cfc145d040f02e37317954db095c839e21f93f5
George Mendoza <gsmendoza@gmail.com>
2013-01-07 15:41:49 +0800

    User should apply the mask plugin to a password field, instead of a text field.

--- jasmine-standalone-1.3.1/spec/jQueryMaskSpec.js
+++ jasmine-standalone-1.3.1/spec/jQueryMaskSpec.js
@@ -1,4 +1,16 @@
 describe("jquery.mask", function() {
+  describe("$(password_field).mask()", function(){
+    it("should add a unmasked version of password_field", function(){
+      var fixture = $('<div id="fixture"/>');
+      
+      fixture.append("<input type='password' id='password-field' />");
+      fixture.append("<input type='checkbox' id='password-field-toggler-1' />");
+      fixture.find('#password-field').mask({toggle: '#password-field-toggler-1'});
+      
+      expect(fixture.find('#password-field').next('#password-field_unmasked').attr('type')).toEqual('text');
+    });
+  });
+
   describe("$(element).mask()", function(){
     it("should work within a detached jQuery element", function(){
       var fixture = $('<div id="fixture"/>');
@@ -7,7 +19,7 @@ describe("jquery.mask", function() {
       fixture.append("<input type='checkbox' id='password-field-toggler-1' />");
       fixture.find('#password-field').mask({toggle: '#password-field-toggler-1'});
       
-      fixture.find('#password-field').next('#password-field_masked').val('test').trigger('change');
+      fixture.find('#password-field').next('#password-field_unmasked').val('test').trigger('change');
       
       expect(fixture.find('#password-field').val()).toEqual('test');
     });
