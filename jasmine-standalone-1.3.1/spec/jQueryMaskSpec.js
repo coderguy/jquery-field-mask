439efb765326f45176eb4f5ec8243ad2f6a5ccfd
George Mendoza <gsmendoza@gmail.com>
2013-01-07 15:46:05 +0800

    Plugin should prevent the browser from autocompleting the password field because the unmasked text field won't be able to pick up the password field's autocompleted value.

--- jasmine-standalone-1.3.1/spec/jQueryMaskSpec.js
+++ jasmine-standalone-1.3.1/spec/jQueryMaskSpec.js
@@ -10,6 +10,18 @@ describe("jquery.mask", function() {
       expect(fixture.find('#password-field').next('#password-field_unmasked').attr('type')).toEqual('text');
     });
   });
+  
+  describe("$(password_field).mask()", function(){
+    it("should disable autocomplete on the password field because the unmasked field won't be able to pick up the password field's value", function(){
+      var fixture = $('<div id="fixture"/>');
+      
+      fixture.append("<input type='password' id='password-field' autocomplete='on' />");
+      fixture.append("<input type='checkbox' id='password-field-toggler-1' />");
+      fixture.find('#password-field').mask({toggle: '#password-field-toggler-1'});
+      
+      expect(fixture.find('#password-field').attr('autocomplete')).toEqual('off');
+    });
+  });
 
   describe("$(element).mask()", function(){
     it("should work within a detached jQuery element", function(){
