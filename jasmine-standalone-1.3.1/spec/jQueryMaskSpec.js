149d9ae64b8f7b2e37b84c01cd9188aabc5a95ea
George Mendoza <gsmendoza@gmail.com>
2013-01-07 15:27:59 +0800

    Plugin should be able to work within a detached jQuery element.

--- jasmine-standalone-1.3.1/spec/jQueryMaskSpec.js
+++ jasmine-standalone-1.3.1/spec/jQueryMaskSpec.js
@@ -1,5 +1,15 @@
 describe("jquery.mask", function() {
-  it("should work", function() {
-    expect(true).toBeTruthy;
+  describe("$(element).mask()", function(){
+    it("should work within a detached jQuery element", function(){
+      var fixture = $('<div id="fixture"/>');
+      
+      fixture.append("<input type='text' id='password-field' />");
+      fixture.append("<input type='checkbox' id='password-field-toggler-1' />");
+      fixture.find('#password-field').mask({toggle: '#password-field-toggler-1'});
+      
+      fixture.find('#password-field').next('#password-field_masked').val('test').trigger('change');
+      
+      expect(fixture.find('#password-field').val()).toEqual('test');
+    });
   });
 });
